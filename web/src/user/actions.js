20923565247a52c6a314d1e7d87a992c6d3c2788
ehmatthes <912463159a@gmail.com>
2019-04-15 12:26:04 +0800

    自动提交

--- web/src/user/actions.js
+++ web/src/user/actions.js
@@ -30,7 +30,7 @@ const dispatchIfValidPublic = (dispatch, reqId, name) => (action, currentReqId) 
 }
 
 //正常的登录，传递给mapDispatchToProps的
-export const login = (userName, password, withDataBool,shortTimeBool, callback, callbackForError) => (dispatch, getState) => {
+export const login = (userName, password, withoutDataBool,shortTimeBool, callback, callbackForError) => (dispatch, getState) => {
   const loginReqId = ++REQ_ID.currentLoginReqId;
 
   const dispatchIfValid = dispatchIfValidPublic(dispatch, loginReqId, 'currentLoginReqId');
