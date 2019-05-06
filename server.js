4803e45439264ea532b5a2ec2900901a3d72dec6
ehmatthes <912463159a@gmail.com>
2019-05-06 10:50:41 +0800

    自动提交

--- server.js
+++ server.js
@@ -412,6 +412,7 @@ app_get('/')
 //检测是否已经登录
 app_get('/api/login')
   .then((req, res, next) => {
+    console.log(req);
     if (!req.session.userName) {
       res.send(JSON.stringify({ code: 0 }));
     } else {
